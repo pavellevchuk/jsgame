93b0cecc4983151551a07ebf0f68a6693049709c
Pavel Levchuk <pavellevchukbg@gmail.com>
2019-01-08 00:38:18 +0200

    Weird Actor

--- game.js
+++ game.js
@@ -22,18 +22,18 @@ class Vector {
   }
 }
 
-const start = new Vector(30, 50);
-const moveTo = new Vector(5, 10);
-const finish = start.plus(moveTo.times(2));
-
-console.log(`Исходное расположение: ${start.x}:${start.y}`);
-console.log(`Текущее расположение: ${finish.x}:${finish.y}`);
+// const start = new Vector(30, 50);
+// const moveTo = new Vector(5, 10);
+// const finish = start.plus(moveTo.times(2));
+//
+// console.log(`Исходное расположение: ${start.x}:${start.y}`);
+// console.log(`Текущее расположение: ${finish.x}:${finish.y}`);
 
 
 class Actor {
   constructor(pos, size, speed) {
     if (pos == undefined) {
-      this.pos = new Vector;
+      this.pos = new Vector();
     } else {
       if (!(pos instanceof Vector)) {
         throw Error("Можно создавать движущийся обьект только с помощью векторов");
@@ -51,7 +51,7 @@ class Actor {
       this.size = size;
     }
     if (speed == undefined) {
-      this.speed = new Vector;
+      this.speed = new Vector();
     } else {
       if (!(speed instanceof Vector)) {
         throw Error("Можно создавать движущийся обьект только с помощью векторов");
@@ -80,18 +80,45 @@ class Actor {
     if (!(movingObj instanceof Actor)) {
       throw Error("Можно передавать только обьект типа Actor");
     }
-
-    if (this == movingObj || this.right == movingObj.left || this.left == movingObj.right ||
-      this.top == movingObj.bottom || this.bottom == movingObj.top) {
+    if (this == movingObj) {
       return false;
     }
-
-    if ((movingObj.left > this.right) || (this.left > movingObj.right) || (this.bottom > movingObj.top) || (this.top < movingObj.bottom)) {
+    if (!(movingObj.left >= this.right ||
+        movingObj.right <= this.left ||
+        movingObj.top >= this.bottom ||
+        movingObj.bottom <= this.top)) {
+      return true;
+    }
       return false;
-    }
-
-    if ((this.left < movingObj.right) && (this.right > movingObj.left) && (this.bottom < movingObj.top) && (this.top > movingObj.bottom)) {
-        return true;
-      }
-    }
   }
+}
+
+
+const items = new Map();
+const player = new Actor();
+items.set('Игрок', player);
+items.set('Первая монета', new Actor(new Vector(10, 10)));
+items.set('Вторая монета', new Actor(new Vector(15, 5)));
+
+function position(item) {
+  return ['left', 'top', 'right', 'bottom']
+    .map(side => `${side}: ${item[side]}`)
+    .join(', ');
+}
+
+function movePlayer(x, y) {
+  player.pos = player.pos.plus(new Vector(x, y));
+}
+
+function status(item, title) {
+  console.log(`${title}: ${position(item)}`);
+  if (player.isIntersect(item)) {
+    console.log(`Игрок подобрал ${title}`);
+  }
+}
+
+items.forEach(status);
+movePlayer(10, 10);
+items.forEach(status);
+movePlayer(5, -5);
+items.forEach(status);
