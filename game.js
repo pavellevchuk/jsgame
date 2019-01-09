333c63aee68ce6dd9fac9a7502a1a6288821888d
Pavel Levchuk <pavellevchukbg@gmail.com>
2019-01-09 22:56:51 +0200

    To actorAt

--- game.js
+++ game.js
@@ -36,7 +36,7 @@ class Actor {
       this.pos = new Vector();
     } else {
       if (!(pos instanceof Vector)) {
-        throw Error("Можно создавать движущийся обьект только с помощью векторов");
+      throw Error(`${pos} не является вектором`);
       }
       // this.pos = new Vector(pos.x, pos.y);
       this.pos = pos;
@@ -45,7 +45,7 @@ class Actor {
       this.size = new Vector(1, 1);
     } else {
       if (!(size instanceof Vector)) {
-        throw Error("Можно создавать движущийся обьект только с помощью векторов");
+        throw Error(`${size} не является вектором`);
       }
       // this.size = new Vector(size.x, size.y);
       this.size = size;
@@ -54,17 +54,32 @@ class Actor {
       this.speed = new Vector();
     } else {
       if (!(speed instanceof Vector)) {
-        throw Error("Можно создавать движущийся обьект только с помощью векторов");
+        throw Error(`${speed} не является вектором`);
       }
       // this.speed = new Vector(speed.x, speed.y);
       this.speed = speed;
     }
 
-    this.bottom = this.pos.y + this.size.y;
-    this.right = this.pos.x + this.size.x;
-    this.top = this.pos.y;
-    this.left =  this.pos.x;
-
+    Object.defineProperty(this, 'left', {
+      get: function() {
+        return this.pos.x
+      }
+    });
+    Object.defineProperty(this, 'top', {
+      get: function() {
+        return this.pos.y;
+      }
+    });
+    Object.defineProperty(this, 'right', {
+      get: function() {
+        return this.pos.x + this.size.x;
+      }
+    });
+    Object.defineProperty(this, 'bottom', {
+      get: function() {
+        return this.pos.y + this.size.y;
+      }
+    });
     Object.defineProperty(this, 'type', {
       value: 'actor'
     });
@@ -72,7 +87,7 @@ class Actor {
   act() {}
   isIntersect(movingObj) {
     if (!(movingObj instanceof Actor)) {
-      throw Error("Можно передавать только обьект типа Actor");
+      throw Error(`${movingObj} не является наследником Actor`);
     }
     if (this == movingObj) {
       return false;
@@ -83,36 +98,90 @@ class Actor {
         movingObj.bottom <= this.top)) {
       return true;
     }
-      return false;
+    return false;
   }
 }
 
 
-const items = new Map();
-const player = new Actor();
-items.set('Игрок', player);
-items.set('Первая монета', new Actor(new Vector(10, 10)));
-items.set('Вторая монета', new Actor(new Vector(15, 5)));
+// const items = new Map();
+// const player = new Actor();
+// items.set('Игрок', player);
+// items.set('Первая монета', new Actor(new Vector(10, 10)));
+// items.set('Вторая монета', new Actor(new Vector(15, 5)));
+//
+// function position(item) {
+//   return ['left', 'top', 'right', 'bottom']
+//     .map(side => `${side}: ${item[side]}`)
+//     .join(', ');
+// }
+//
+// function movePlayer(x, y) {
+//   player.pos = player.pos.plus(new Vector(x, y));
+// }
+//
+// function status(item, title) {
+//   console.log(`${title}: ${position(item)}`);
+//   if (player.isIntersect(item)) {
+//     console.log(`Игрок подобрал ${title}`);
+//   }
+// }
+//
+// items.forEach(status);
+// movePlayer(10, 10);
+// items.forEach(status);
+// movePlayer(5, -5);
+// items.forEach(status);
 
-function position(item) {
-  return ['left', 'top', 'right', 'bottom']
-    .map(side => `${side}: ${item[side]}`)
-    .join(', ');
-}
 
-function movePlayer(x, y) {
-  player.pos = player.pos.plus(new Vector(x, y));
-}
-
-function status(item, title) {
-  console.log(`${title}: ${position(item)}`);
-  if (player.isIntersect(item)) {
-    console.log(`Игрок подобрал ${title}`);
+class Level {
+  constructor(mesh, actors) {
+    this.grid = mesh;
+    if (mesh == undefined) {
+      this.height = 0;
+      this.width = 0;
+    } else {
+      this.height = mesh.length;
+      Object.defineProperty(this, 'width', {
+        get: function() {
+          return Math.max(...(mesh.map(row => row.length)));
+        }
+      });
+    }
+    this.status = null;
+    this.finishDelay = 1;
+    this.actors = actors;
+    Object.defineProperty(this, 'player', {
+      get: function() {
+        return this.actors.find(function(act) {
+          return act.type === 'player';
+        });
+      }
+    });
   }
-}
-
-items.forEach(status);
-movePlayer(10, 10);
-items.forEach(status);
-movePlayer(5, -5);
-items.forEach(status);
+  isFinished(){
+    if(this.status != null && this.finishDelay < 0){
+      return true;
+    }
+    return false;
+  }
+  actorAt(movingObj){
+    if(!(movingObj instanceof Actor)){
+      throw Error(`${movingObj} не является наследником Actor`);
+    }
+    if(this.grid == undefined || this.actors.length == 1){
+      return undefined;
+    }
+    return this.actors.find(function(act){
+      return movingObj.isIntersect(act);
+    });
+  }
+  obstacleAt(moveTo,size){
+    if(this.grid == undefined || this.actors.length == 1){
+      return undefined;
+    }
+    if(!(moveTo instanceof Vector) || !(size instanceof Vector)){
+      throw Error('Один из аргументов не является вектором');
+    }
+    
+    }
+  }
